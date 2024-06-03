86e887f60138c91cad7f618e96d247e2eece8b20
WeiTingLai <85389936+WeiTingLai@users.noreply.github.com>
2024-06-03 10:19:42 +0800

    VB_NAR.R

--- VB_NAR.R
+++ VB_NAR.R
@@ -798,7 +798,7 @@ TPR_FPR_TCR<-function(object,coef_beta,beta,Iteration,current=F)
       beta =beta[-c(1:m),]
     }
   }
-
+    coef_beta<-matrix(coef_beta,nrow=Iteration)
     beta<-as.matrix(beta)
     phi_true<-unlist(sapply(1:(2*p*m),function(j)ifelse(j%%2!=0,ifelse(beta[j%/%2+1,ifelse((j%/%2+1)%%ncol(Y)==0,ncol(Y),(j%/%2+1)%%ncol(Y))]==0,0,1),list(sapply(1:length(fit$segment[[j%/%2]]),function(s)ifelse(sum(abs(beta[j%/%2,ifelse(j%/%2%%ncol(Y)==0,-ncol(Y),-(j%/%2%%ncol(Y)))][c((sum(fit$segment[[j%/%2]][c(1:s)])-fit$segment[[j%/%2]][[s]]+1):sum(fit$segment[[j%/%2]][c(1:s)]))]))==0,0,1)))) ))
     TP<-sapply(1:Iteration,function(j)sum(ifelse(abs(coef_beta[j,which(c(abs(beta[c(1:(m*p)),]))>0)])>0,1,0)))
